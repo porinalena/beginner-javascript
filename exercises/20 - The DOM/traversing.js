19ac21283a76556272255d273a1bb416db3de739
Lena Porina <porinalena@gmail.com>
2019-12-06 23:07:23 -0800

    Traversing and removing elements in the DOM

---
+++ exercises/20 - The DOM/traversing.js
@@ -0,0 +1,17 @@
+console.log('It works');
+const wes = document.querySelector('.wes');
+console.log(wes.children);
+console.log(wes.childNodes);
+console.log(wes.firstElementChild);
+console.log(wes.lastElementChild);
+console.log(wes.previousElementSibling);
+console.log(wes.nextElementSibling);
+console.log(wes.parentElement);
+
+const p = document.createElement('p');
+p.textContent = 'I will be removed';
+document.body.appendChild(p);
+
+p.remove();
+
+console.log(p);
