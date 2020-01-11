38f3c61f522d8b9b436a8f3ac26054855d6a4db5
Lena Porina <porinalena@gmail.com>
2020-01-10 23:02:00 -0800

    Finished prevent default and form events lesson

---
+++ exercises/29 - Events/forms.js
@@ -0,0 +1,32 @@
+const wes = document.querySelector('.wes');
+
+wes.addEventListener('click', function(event) {
+  console.log('You clicked it');
+  const shouldChangePage = confirm(
+    'This website might be malicious. Do you wish to proceed?'
+  );
+  if (!shouldChangePage) {
+    event.preventDefault();
+    // window.location = event.currentTarget.href;
+  }
+  console.log(shouldChangePage);
+});
+
+const signUpForm = document.querySelector('[name="signup"]');
+signUpForm.addEventListener('submit', function(event) {
+  const name = event.currentTarget.name.value;
+  if (name.includes('chad')) {
+    alert('Sorry bro');
+    event.preventDefault();
+  }
+});
+
+function logEvent(event) {
+  console.log(event.type);
+  console.log(event.currentTarget.value);
+}
+
+signUpForm.name.addEventListener('keyup', logEvent);
+signUpForm.name.addEventListener('keydown', logEvent);
+signUpForm.name.addEventListener('focus', logEvent);
+signUpForm.name.addEventListener('blur', logEvent);
