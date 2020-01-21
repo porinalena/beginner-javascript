048a5119c0bfcf00a4f51bb382f7f32313b38d00
Lena Porina <porinalena@gmail.com>
2020-01-20 20:41:09 -0800

    Started working on the Accept terms button getting activated on a scroll event

--- exercises/35 - Scroll To Accept/scroll-to-accept.js
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -1,1 +1,18 @@
 console.log('IT WORKS!');
+
+const watch = document.querySelector('.watch');
+const acceptButton = document.querySelector('.accept');
+
+function observerCallback() {
+  acceptButton.removeAttribute('disabled');
+}
+
+const observer = new IntersectionObserver(observerCallback);
+
+watch.addEventListener('scroll', event => {
+  console.log('I just passed watch element');
+  console.log(event.currentTarget);
+  console.log(observer);
+});
+
+observer.observe(watch);
